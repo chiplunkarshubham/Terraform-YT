9c3c139f5e1648da367b928acf9c1a8b1f17c96f
chiplunkarshubham <52418126+chiplunkarshubham@users.noreply.github.com>
2022-09-06 14:41:25 +0530

    Update datatype-demo.tf

--- Exercises/datatypes/datatype-demo.tf
+++ Exercises/datatypes/datatype-demo.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4FK6WH3EETWH2S6Z"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 # resource "aws_iam_user" "myuser" {
